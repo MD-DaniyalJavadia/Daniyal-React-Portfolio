1dc2130cca120caddd8407071c5b186e94f121f1
MD-Daniyal Javadia <155415513+MD-DaniyalJavadia@users.noreply.github.com>
2026-03-04 09:57:49 +0500

    Integrate Helmet for SEO and add skills metadata
    Added Helmet for SEO optimization and structured data.

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,28 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import ProjectShowcase from './components/ProjectShowCase'
+import { Helmet } from "react-helmet-async";
 
 
 function App() {
+  const skills = ["React", "Node.js", "Laravel", ".NET", "Flutter", "UI/UX Design"];
 
   return (
     <>
+      <Helmet>
+        <title>Daniyal Javadia - Full Stack Web Developer | .NET & Flutter Developer | UI/UX Designer</title>
+        <meta name="description" content="Full Stack Web Developer, .NET Developer, Flutter Developer, and UI/UX Designer. Explore portfolio projects and tech blog." />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Daniyal Javadia",
+            "url": "https://daniyal-works-portfolio.vercel.app/",
+            "jobTitle": "Full Stack Web Developer | .NET Developer | Flutter Developer | UI/UX Designer",
+            "skills": ["React", "Node.js", "Laravel", ".NET", "Flutter", "UI/UX Design"]
+          })}
+        </script>
+      </Helmet>
       <Header />
       <Hero />
       <ThingsILove />
@@ -23,8 +39,8 @@ function App() {
       <Skills />
       <Tools />
       <Certificates />
-      <Projects/>
-      <ProjectShowcase/>
+      <Projects />
+      <ProjectShowcase />
       <Contact />
       <Footer />
     </>
